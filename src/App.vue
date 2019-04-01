2c3df4a017471f3c417a39aeecdd237eb3cdfe24
Walker <walker@incuca.com.br>
2019-04-01 12:06:01 -0300

    add template demo

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,44 @@
 <template>
   <div id="app">
-    <img
-      alt="Vue logo"
-      src="./assets/logo.png"
-    >
-    <h1>Welcome to Your Plugin in Vue.js</h1>
-    <p>add: 1 + 1 = {{ $add(1, 1) }}</p>
+    <h1>ic-vslider demo</h1>
+    <ic-vslider title="Muito Além do Trabalho">
+      <ic-vslider-slider title="Pomar" subtitle="Horta comunitária">
+        <div slot="featured">
+          <img src="https://placekitten.com/320/320" alt="kitty">
+        </div>
+        <div slot="content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Maecenas tempus, urna sodales condimentum eleifend,
+          metus dui suscipit diam, eget consequat augue libero vel
+          purus. In sed efficitur libero. Aenean scelerisque leo
+          sed dolor hendrerit, id gravida lorem mattis. Etiam
+          laoreet urna sit amet posuere ultricies. Cras hendrerit
+          vel velit at varius. Duis dictum tellus vel erat
+          pellentesque, vel commodo ipsum varius.
+        </div>
+      </ic-vslider-slider>
+      <ic-vslider-slider title="Pomar" subtitle="Horta comunitária">
+        <div slot="featured">
+          <iframe width="610" height="343" src="https://www.youtube.com/embed/k4mJkt1y7E8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        <div slot="content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Maecenas tempus, urna sodales condimentum eleifend,
+          metus dui suscipit diam, eget consequat augue libero vel
+          purus. In sed efficitur libero. Aenean scelerisque leo
+          sed dolor hendrerit, id gravida lorem mattis. Etiam
+          laoreet urna sit amet posuere ultricies. Cras hendrerit
+          vel velit at varius. Duis dictum tellus vel erat
+          pellentesque, vel commodo ipsum varius.
+        </div>
+      </ic-vslider-slider>
+    </ic-vslider>
   </div>
 </template>
 
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/ic-vslider.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="ic-vslider">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IcVSlider'
+};
+</script>
+
+<style lang="css" scoped>
+
+</style>
